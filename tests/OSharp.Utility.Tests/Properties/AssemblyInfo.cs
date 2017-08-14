f76e32c3bdea03a0476112a641bc86a15cee9446
郭明锋 <gmf520@yeah.net>
2017-08-14 13:35:57 +0800

    初始化代码库，添加工具组件项目OSharp.Utility及其测试OSharp.Utility.Tests项目

---
+++ tests/OSharp.Utility.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OSharp.Utility.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("OSharp.Utility.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1431f35b-3a08-40ac-b8ae-c995ec34191c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/OSharp.Utility.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OSharp.Utility.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod01()
+        {
+            Assert.True(true);
+        }
+    }
+}
